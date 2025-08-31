87b488c93a2eef469c1428315bc7eb34bc58cce2
Sahil-Kumar7253 <darkslayer7253@gmail.com>
2025-08-31 20:20:01 +0530

    Implemented Middleware

---
+++ backend/Middleware/authMiddleware.js
@@ -0,0 +1,19 @@
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+async function authMiddleware(req, res, next) {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1]; // Bearer TOKEN
+    if (!token) {
+        return res.status(401).json({ error: 'Access denied. No token provided.' });
+    }
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = decoded.user;
+        next();
+    } catch (err) {
+        res.status(400).json({ error: 'Invalid token.' });
+    }
+}
+
+module.exports = authMiddleware;
