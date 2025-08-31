1b2da95512a922dea862bf424ae990774ae6e1d0
Sahil-Kumar7253 <darkslayer7253@gmail.com>
2025-08-31 20:27:24 +0530

    Implemented Endpoint For authentication

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const config = require('./config');
 const cors = require('cors');
+const authRoutes = require('./Routes/authRoutes');
 
 port = config.PORT;
 
@@ -12,5 +13,7 @@ app.use(express.urlencoded({ extended: true }));
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
+
+app.use('/api/auth', authRoutes);
 
 module.exports = app;
